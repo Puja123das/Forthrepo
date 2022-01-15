0b574d2e72bd96af02cf696ad3333da9a337869a
Puja123das <81166085+Puja123das@users.noreply.github.com>
2022-01-15 20:47:38 +0530

    Update hello4.java

--- hello4.java
+++ hello4.java
@@ -3,7 +3,7 @@ static public void main(String... args){
 <<<<<<< HEAD
 System.out.println("hello java7");  
 =======
-System.out.println("hello java6");  
+System.out.println( hii satya");  
 >>>>>>> 7ac504a60ce8d0e3d0104e1680398caceb727559
 }  
 };  
